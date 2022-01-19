c2c9121a0dde11cf67aaf6829b753f3b64b6bbf1
Danil Zinurov <d.zinurov@belko.tech>
2022-01-20 02:23:53 +0300

    First commit

---
+++ vendor/signup.php
@@ -0,0 +1,124 @@
+<?php
+
+session_start();
+require_once 'connect.php';
+/*---------------------------------------------------------------------------------------- */
+
+$full_name = $_POST['full_name'];
+$login = $_POST['login'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$password_confirm = $_POST['password_confirm'];
+$check_login = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login'");    
+// $name_valid = "/^[а-яёА-ЯЁ\s]+$/";
+/*---------------------------------------------------------------------------------------- */
+
+if (mysqli_num_rows($check_login) > 0) {
+    $response = [
+        "status" => false,
+        "type" => 1,
+        "message" => "Такой логин уже существует",
+        "fields" => ['login']
+    ];
+
+    echo json_encode($response);
+    die;
+}
+
+if (!empty(mb_strlen($login) < 6)){
+    $response = [
+        "status" => false,
+        "type" => 1,
+        "message" => "Login должен быть более 6-ти символов",
+        "fields" => ['login']
+    ];
+
+    echo json_encode($response);
+    die;
+}
+
+// if (!preg_match($name_valid, $full_name)){
+//     $response = [
+//         "status" => false,
+//         "type" => 1,
+//         "message" => "ФИО должны состоять только из букв русского и английского алфавита",
+//         "fields" => ['full_name']
+//     ];
+
+//     echo json_encode($response);
+//     die;
+// }
+
+/*---------------------------------------------------------------------------------------- */
+$error_fields = [];
+
+if ($login === '') {
+    $error_fields[] = 'login';
+}
+if ($password === '') {
+    $error_fields[] = 'password';
+}
+
+if ($full_name === '') {
+    $error_fields[] = 'full_name';
+}
+
+if ($email === '' || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $error_fields[] = 'email';
+}
+
+if ($password_confirm === '') {
+    $error_fields[] = 'password_confirm';
+}
+
+if (!$_FILES['avatar']) {
+    $error_fields[] = 'avatar';
+}
+/*---------------------------------------------------------------------------------------- */
+
+if (!empty($error_fields)) {
+    $response = [
+        "status" => false,
+        "type" => 1,
+        "message" => "Проверьте правильность полей",
+        "fields" => $error_fields
+    ];
+
+    echo json_encode($response);
+
+    die();
+}
+
+if ($password === $password_confirm) {
+
+    $path = 'uploads/' . time() . $_FILES['avatar']['name'];
+    if (!move_uploaded_file($_FILES['avatar']['tmp_name'], '../' . $path)) {
+        $response = [
+            "status" => false,
+            "type" => 2,
+            "message" => "Ошибка при загрузке аватарки",
+        ];
+        echo json_encode($response);
+    }
+/*---------------------------------------------------------------------------------------- */
+    $password = md5($password); // Кодировка пароля MD 5
+/*---------------------------------------------------------------------------------------- */
+    mysqli_query($connect, "INSERT INTO `users` (`id`, `full_name`, `login`, `email`, `password`, `avatar`) VALUES (NULL, '$full_name', '$login', '$email', '$password', '$path')");
+
+    $response = [
+        "status" => true,
+        "message" => "Регистрация прошла успешно!",
+    ];
+    echo json_encode($response);
+
+} else {
+    $response = [
+        "status" => false,
+        "message" => "Пароли не совпадают",
+    ];
+    echo json_encode($response);
+}
+
+/*---------------------------------------------------------------------------------------- */
+
+?>
